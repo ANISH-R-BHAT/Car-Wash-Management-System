62487c8330b4fdd057dcbe6869d882f902b5b1b8
psergicv <sv.postolachi@gmail.com>
2022-10-19 22:08:33 +0300

    minor change

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = "--------"
 app.config['MYSQL_DB'] = "carwash_db2"
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "Post0lach123"
+app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
